49a74031c07fe6e974b1fcf8a21d793490e07aad
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2022-12-26 22:20:37 -0800

    added signup, login, and logout routes and controllers

---
+++ controllers/posts.js
@@ -0,0 +1,13 @@
+const cloudinary = require("../middleware/cloudinary");
+const Post = require("../models/Post");
+
+module.exports = {
+	getProfile: async (req, res) => {
+		try {
+			const posts = await Post.find({ user: req.user.id });
+			res.render('profile.ejs', { posts: posts, user: req.user });
+		} catch(error) {
+			console.log(error);
+		}
+	},
+};
