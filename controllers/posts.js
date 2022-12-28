c117b60f6a73a7f2861acd72b843a0f501f2ce69
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2022-12-27 21:43:16 -0800

    added the ability to create a post and view individual posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -18,4 +18,31 @@ module.exports = {
 			console.log(error);
 		}
 	},
+	createPost: async (req, res) => {
+		try {
+			// Upload image to cloudinary
+			const result = await cloudinary.uploader.upload(req.file.path);
+
+			await Post.create({
+				title: req.body.title,
+				image: result.secure_url,
+				cloudinaryId: result.public_id,
+				caption: req.body.caption,
+				likes: 0,
+				user: req.user.id,
+			});
+			console.log("Post has been added!");
+			res.redirect("/profile");
+		} catch (error) {
+			console.log(error);
+		}
+	},
+	getPost: async (req, res) => {
+		try {
+			const post = await Post.findById(req.params.id);
+			res.render('post.ejs', { post: post, user: req.user });
+		} catch (error) {
+			console.log(error);
+		}
+	},
 };
